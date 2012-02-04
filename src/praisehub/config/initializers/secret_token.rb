34aa843cedfe8eabaa057e64c6ddf3ca3c4b6d5d
Eduardo Ferreira <eafonsof@yahoo.com>
2012-02-04 17:27:24 -0500

    Here you go, the new app. Basically just a result of rails new. Nothing added yet.

---
+++ src/praisehub/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Praisehub::Application.config.secret_token = '23e6b5d0586a85e9575c4495749b3e2d0725bad9114aaaccb2dbec0af0aeaef2d915427f8bea1f8f083862b6d1210afa50d8e1d6cb822ef29db7b9289f00acc7'
