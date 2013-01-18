4be6d283fd4ddf788061f3211509e4414b0414f0
benpfohl <benpfohl@truenorthwebdesign.com>
2013-01-18 15:12:02 -0500

    Empty this out - Remaking this as a PHP codeigniter project

--- src/praisehub/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Praisehub::Application.config.session_store :cookie_store, key: '_praisehub_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Praisehub::Application.config.session_store :active_record_store
