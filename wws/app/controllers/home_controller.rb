f1bb9819935b86066b4009efc9dd4796717d374d
Eduardo Ferreira <Eduardo.Ferreira@Turner.com>
2013-02-19 00:39:59 -0500

    Add sample rails app with some use of dashboard, login, etc.

---
+++ wws/app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def dashboard
+  end
+
+  def login
+  end
+
+  def all_events
+  end
+
+  def my_events
+  end
+
+  def new_event
+  end
+end
---
+++ wws/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wws::Application.config.secret_token = '6fcb6335583ac18894d7dd2692aa66ef85ce400f8797fc35577deccbde7cba792a796d50418580e3728155b86f6d58a72004b8d8ddf7e1f262a2573a872d79ff'
